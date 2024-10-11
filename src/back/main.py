1c49af38cd7086383cef3a52c8aa6832fb3e9252
lexie_con <Lexie_Li@outlook.com>
2024-10-11 18:22:03 +0800

    增加了基础的python后台

---
+++ src/back/main.py
@@ -0,0 +1,13 @@
+from flask import Flask
+from flask_cors import CORS
+from data import *
+
+app = Flask(__name__)
+CORS(app, supports_credentials=True)
+
+@app.route("/array")
+def showArray():
+    return str(dict), 200, {"ContentType": "application/json"}
+
+if __name__ == "__main__":
+    app.run()
